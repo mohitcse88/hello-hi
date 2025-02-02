b7891c3a93cdec5d9124474050bc0bab10acc748
Mohit kumar <104765231+mohitcse88@users.noreply.github.com>
2025-02-02 03:57:08 +0000

    javascript variable

---
+++ hello.js
@@ -0,0 +1,28 @@
+console.log("Hello World!");
+
+console.log("I am Mohit Kumar.");
+
+// variable - Declaration, Initialization 
+// In JavaScript, Instructions are called Statements, When it's separated by semicolons(;).
+// Automatic Insertion of semicolons
+// Variable as a symbolic name for value in your application. The name of variables called identifiers.
+// Identifier is a sequence of character in the code that identifies a variable, function and property. 
+
+let firstName = "Mohit"
+let lastName = "Kumar"
+let emailId = "mohitkumardoctor@gmail.com"
+let phoneNo = "6200328165"
+let age = "18" 
+let country = "India"
+let state = "Bihar"
+let district = "East Champaran"
+let city = "Motihari"
+let loggedIn = true
+let debitCard = true 
+
+
+
+console.log(`First Name: ${firstName} and Last Name: ${lastName}`);
+console.table([firstName, lastName, emailId, phoneNo, age, country, state, district, city, loggedIn, debitCard])
+
+
